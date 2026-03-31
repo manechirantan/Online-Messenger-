a58b393cd806712e65a6c07822410445f562688f
Chirantan Mane <chirantanmane0@gmail.com>
2026-03-31 10:51:15 +0530

    updates

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import { fileURLToPath } from "url";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+app.get("/favicon.ico", (req, res) => res.status(204));
 import mongoose from "mongoose";
 async function main() {
   await mongoose.connect("mongodb://127.0.0.1:27017/whatsapp");
@@ -19,22 +22,39 @@ main()
     console.log(err);
   });
 
-let chat = new Chat({
-  from: "sreeleela",
-  to: "chiru",
-  msg: "how are you, what are you doing",
-  createdAt: new Date(),
+app.get("/", async (req, res) => {
+  let chats = await Chat.find();
+  res.render("index.ejs", { chats });
 });
 
-chat
-  .save()
-  .then((res) => {
-    console.log(res);
-  })
-  .catch((err) => {
-    console.log(err);
+app.get("/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.post("/", async (req, res) => {
+  let { form, msg, to } = req.body;
+  let newChat = new Chat({
+    from: form,
+    msg: msg,
+    to: to,
+    createdAt: new Date(),
   });
+  try {
+    let chati = await newChat.save();
+    console.log(chati);
+  } catch (error) {
+    console.log(error);
+  }
+  res.redirect("/");
+});
+
+// app.get("/:id",async (req, res) => {
+//   let { id } = req.params;
+//   let user= await Chat.findOne({_id:id})
+//   console.log(user)
+//   res.render('chat.ejs',{user})
+// });
 
 app.listen(port, () => {
-  console.log("connected to database easily lale");
+  console.log("connected to server easily");
 });
